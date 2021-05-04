7e24a16036acd16e4b9dda2d3ea4553b8813f672
Kabewh <73640269+Kabewh@users.noreply.github.com>
2021-05-04 19:31:05 +0300

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,30 +1,58 @@
 #include "game.h"
-#include <iostream>
-#include<string>
-#include<iomanip>
-
 using namespace std;
 game::game()
 {
-	
+	choice1 = 0;
+	choice2 = 0;
+	playing = true;
 }
 void game::mainMenu()
 {
-	choice1 = 0;
+	
 	cout << "\n";
 	cout << setw(67);
-	cout << "The Adventurier\n\n" << endl;
+	cout << "The Adventurer\n\n\n";
 	cout << "Press one of the following buttons to: \n";
 	cout << "1. Start your adventure!\n";
-	cout << "2. Exit\n";
+	cout << "0. Exit\n";
 	cout << "Enter: ";
 	cin >> choice1;
-	if (choice1 == 1)
+	switch (choice1)
 	{
+	case 1:
 		cout << "You just begun your adventure! " << endl;
-	}
-	else if (choice1 == 2)
-	{
+		break;
+	case 0:
 		cout << "I'm sorry to see you go, bye bye...";
+		playing = false;
+		break;
+	default:
+		break;
+	}
+	
+}
+
+/*void game::combatMenu()
+{
+	while (if player is in combat print the following menu)
+	{
+		cout << "Choose what you want to do: " << endl;
+		cout << "1. Attack" << endl;
+		cout << "2. Block" << endl;
+		cout << "3. Recover" << endl;
+		break;
 	}
 }
+*/
+/*void game::story()
+{
+	//while game is running, if player movement ++, go to next story
+	//if player movement --, return to the previous story
+	while (choice1 == 1)
+	{
+		if 
+		st++;
+
+
+	}
+}*/
--- game.h
+++ game.h
@@ -1,15 +1,24 @@
 #pragma once
-
+#include <iostream>
+#include<string>
+#include<iomanip>
+#include<ctime>
 
 class game
 {
 	public:
 		game();
 		void mainMenu();
+		//void story();
+		//void combatMenu();
 
+		inline bool getPlaying() const { return this->playing; }
 
 private:
 	int choice1;
+	int choice2;
+	bool playing;
+	int st;
 };
 
 
